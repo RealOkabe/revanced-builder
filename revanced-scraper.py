b0726df6ee4cee38ec1d3fe87225531208112a10
RealOkabe <anmolzagrawal@gmail.com>
2023-06-02 13:58:20 -0400

    Added urls for patches and integrations releases

--- revanced-scraper.py
+++ revanced-scraper.py
@@ -2,9 +2,11 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-URL_GITHUB_API = "https://api.github.com/repos/"
-URL_REVANCED_CLI = URL_GITHUB_API + "revanced/revanced-cli/releases"
+URL_BASE = "https://api.github.com/repos/revanced/"
+URL_REVANCED_CLI = URL_BASE + "revanced-cli/releases"
+URL_REVANCED_PATCHES = URL_BASE + "revanced-patches/releases"
+URL_REVANCED_INTEGRATIONS = URL_BASE + "revanced-integrations/releases"
 
-jsonData = json.loads(requests.get(URL_REVANCED_CLI).text)
-for i in jsonData:
-    print(i['name'])
+cliReleases = json.loads(requests.get(URL_REVANCED_CLI).text)
+patchesReleases = json.loads(requests.get(URL_REVANCED_PATCHES).text)
+integrationsReleases = json.loads(requests.get(URL_REVANCED_INTEGRATIONS).text)
