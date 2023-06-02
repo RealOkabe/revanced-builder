78a98a939cae38f881a2dcc0a5ecf465d215a64b
RealOkabe <anmolzagrawal@gmail.com>
2023-06-02 14:41:12 -0400

    Added function to compare versions
    Also added a placebo to update the files if new versions are available

--- revanced-scraper.py
+++ revanced-scraper.py
@@ -6,14 +6,31 @@ URL_BASE = "https://api.github.com/repos/revanced/"
 URL_REVANCED_CLI = URL_BASE + "revanced-cli/releases"
 URL_REVANCED_PATCHES = URL_BASE + "revanced-patches/releases"
 URL_REVANCED_INTEGRATIONS = URL_BASE + "revanced-integrations/releases"
+VERSIONS_FILE_PATH = "versions.json"
 
-cliReleases = json.loads(requests.get(URL_REVANCED_CLI).text)
-patchesReleases = json.loads(requests.get(URL_REVANCED_PATCHES).text)
-integrationsReleases = json.loads(requests.get(URL_REVANCED_INTEGRATIONS).text)
+def checkFileVersions():
+    # Get the releases
+    cliReleases = json.loads(requests.get(URL_REVANCED_CLI).text)
+    patchesReleases = json.loads(requests.get(URL_REVANCED_PATCHES).text)
+    integrationsReleases = json.loads(requests.get(URL_REVANCED_INTEGRATIONS).text)
 
-VERSIONS_FILE = json.loads(open("versions.json").read())
+    # Get latest versions from releases
+    latestVersionCli = cliReleases[0]['id']
+    latestVersionPatches = patchesReleases[0]['id']
+    latestVersionIntegrations = integrationsReleases[0]['id']
 
-latestVersionCli = VERSIONS_FILE[0]['vCli']
-latestVersionPatches = VERSIONS_FILE[0]['vPatches']
-latestVersionIntegrations = VERSIONS_FILE[0]['vIntegrations']
+    # Get last versions from cache
+    VERSIONS_FILE = json.loads(open(VERSIONS_FILE_PATH).read())
+    lastVersionCli = VERSIONS_FILE[0]['vCli']
+    lastVersionPatches = VERSIONS_FILE[0]['vPatches']
+    lastVersionIntegrations = VERSIONS_FILE[0]['vIntegrations']
+
+    if latestVersionCli > lastVersionCli:
+        updateFiles(URL_REVANCED_CLI)
+
+    if latestVersionPatches > lastVersionPatches:
+        updateFiles(URL_REVANCED_PATCHES)
+    
+    if latestVersionIntegrations > lastVersionIntegrations:
+        updateFiles(URL_REVANCED_INTEGRATIONS)
 
